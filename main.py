075d5836607565ee2859f076136e0c46d3a9a8a2
Maciek Walega <mwaleg.sa@gmail.com>
2021-02-25 04:24:45 +0100

    Sequencing data

--- main.py
+++ main.py
@@ -38,3 +38,26 @@ scaler = scaler.fit(np.expand_dims(train_data, axis=1))
 
 train_data = scaler.transform(np.expand_dims(train_data, axis=1))
 test_data = scaler.transform(np.expand_dims(test_data, axis=1))
+
+# create data sequences
+def create_sequences(data, seq_length):
+    xs = []
+    ys = []
+
+    for i in range(len(data)-seq_length-1):
+        x = data[i:(i+seq_length)]
+        y = data[i+seq_length]
+        xs.append(x)
+        ys.append(y)
+
+    return np.array(xs), np.array(ys)
+
+seq_length = 5
+X_train, y_train = create_sequences(train_data, seq_length)
+X_test, y_test = create_sequences(test_data, seq_length)
+
+X_train = torch.from_numpy(X_train).float()
+y_train = torch.from_numpy(y_train).float()
+
+X_test = torch.from_numpy(X_test).float()
+y_test = torch.from_numpy(y_test).float()
